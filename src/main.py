b52fc8d7a6945245347e2a77cb60dfc8a60e78e8
jericjan <cantilfrederick@gmail.com>
2023-06-04 09:35:07 +0800

    calculating pulls for % chance added

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import itertools
+import json
 import random
 import statistics
+from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 from numpy.random import choice
 
-ROUNDS = 10000
+ROUNDS = 1000
 
 
 def show_histogram(data, title, bin_count=100, cumulative=False, fig_num = None, alpha=1, label=None):    
@@ -13,7 +16,7 @@ def show_histogram(data, title, bin_count=100, cumulative=False, fig_num = None,
         plt.figure()
     else:
         plt.figure(fig_num)
-    plt.hist(data, histtype='stepfilled', bins=bin_count,cumulative=cumulative, density=True if cumulative else False, alpha=alpha, label=label)
+    n, bins, patches = plt.hist(data, histtype='stepfilled', bins=bin_count,cumulative=cumulative, density=True if cumulative else False, alpha=alpha, label=label)
     plt.title(title)
     plt.xlabel("Pulls")
     plt.ylabel("Probablity" if cumulative else "Frequency")
@@ -22,7 +25,7 @@ def show_histogram(data, title, bin_count=100, cumulative=False, fig_num = None,
         ax.legend(prop={'size': 10})
     # if cumulative:  
     #     plt.plot(0, .5, 200, .5, color="red")
-    return plt.gcf().number
+    return n, bins
 
 def pull_valk(amount_wanted=1):
     item_wanted = "valk"
@@ -50,8 +53,7 @@ def pull_valk(amount_wanted=1):
 
         pull_success_results.append(pull_count)
 
-    valk_avg = sum(pull_success_results) / ROUNDS
-    return pull_success_results, valk_avg
+    return pull_success_results
 
 
 def pull_gears(wishing_well=True):
@@ -116,34 +118,45 @@ def pull_gears(wishing_well=True):
         # print(f"Got all gear {items_got} in {pull_count} pulls", end='\r')
         pull_success_results.append(pull_count)
 
-    gear_avg = sum(pull_success_results) / ROUNDS
+    return pull_success_results
 
-    return pull_success_results, gear_avg
+save_file = Path("pull_data.json")
 
+if save_file.exists():
+    with Path("pull_data.json").open() as f:        
+        dic = json.load(f)
+        valk_res = dic['valks']
+        gear_res = dic['gears']
+        no_well_gear_res = dic['no_well_gears']
+else:
 
-valk_res, valk_avg = pull_valk()
-gear_res, gear_avg = pull_gears(wishing_well=True)
-no_well_gear_res, no_well_gear_avg = pull_gears(wishing_well=False)
+    valk_res = pull_valk()
+    gear_res = pull_gears(wishing_well=True)
+    no_well_gear_res = pull_gears(wishing_well=False)
+
+    with Path("pull_data.json").open("w") as f:
+        dic = {"valks":valk_res, "gears":gear_res, "no_well_gears":no_well_gear_res}        
+        json.dump(dic,f)
 
 combined_res = [x+y for x, y in zip(valk_res, gear_res)]
 no_well_combined_res = [x+y for x, y in zip(valk_res, no_well_gear_res)]
 
-valk_xtals = valk_avg * 280
-gear_xtals = gear_avg * 280
+valk_xtals = statistics.mean(valk_res) * 280
+gear_xtals = statistics.mean(gear_res) * 280
 
 def calc_mean_median_mode(data):
     mean = statistics.mean(data)
-    print(f"Mean: {mean} ({mean*280} pulls)")
+    print(f"Mean: {mean} ({mean*280} xtals)")
 
     median = statistics.median(data)
-    print(f"Median: {median} ({median*280} pulls)")
+    print(f"Median: {median} ({median*280} xtals)")
 
     mode = statistics.multimode(data)
-    print(f"Mode: {mode} ({[x*280 for x in mode]}) pulls")
+    print(f"Mode: {mode} ({[x*280 for x in mode]}) xtals")
 
 
-print(f"Average pulls to get valk: {valk_avg} ({valk_xtals} crystals)")
-print(f"Average pulls to get all gear: {gear_avg} ({gear_xtals} crystals)")
+print(f"Average pulls to get valk: {statistics.mean(valk_res)} ({valk_xtals} crystals)")
+print(f"Average pulls to get all gear: {statistics.mean(gear_res)} ({gear_xtals} crystals)")
 print(f"Total crystals to 4/4 on average: {valk_xtals + gear_xtals}")
 print("\n")
 print("Valk statistics:")
@@ -163,10 +176,31 @@ calc_mean_median_mode(no_well_combined_res)
 
 show_histogram(valk_res, "Valk pull successes", 10)
 show_histogram(gear_res, "Stig pull successes w/ wishing well")
-show_histogram(valk_res, "Valk pull successes (cumulative)", cumulative=True)
+values, bins = show_histogram(valk_res, "Valk pull successes (cumulative)", cumulative=True)
 # fig_num = show_histogram(gear_res, "Stig pull successes (cumulative)", cumulative=True, alpha=0.5, label="w/ wishing well")
 # show_histogram(no_well_gear_res, "Stig pull successes (cumulative)", cumulative=True, fig_num=fig_num, alpha=0.5, label="w/o wishing well")
 show_histogram([gear_res, no_well_gear_res], "Stig pull successes (cumulative)", cumulative=True, alpha=0.5, label=["w/ wishing well", "w/o wishing well"])
 
 show_histogram([combined_res, no_well_combined_res], "Combined pull successes (cumulative)", cumulative=True, alpha=0.5, label=["w/ wishing well", "w/o wishing well"])
+
+
+
+class Xcalculator:
+    def __init__(self, values, bins):
+        self.values = values
+        self.bins = bins
+    def calculate_x_value(self, y_value):
+        # cumulative_sum = np.cumsum(self.values) #no need cuz alr cumsum'd
+        # cumulative_sum /= cumulative_sum[-1] 
+        index = np.where(self.values >= y_value)[0][0] #gets first array, then gets first element >= y
+        x_value = self.bins[index]
+        print(f"{x_value} pulls has a {y_value*100}% chance")
+
+xcal = Xcalculator(values, bins)
+xcal.calculate_x_value(0.25)
+xcal.calculate_x_value(0.5)
+xcal.calculate_x_value(0.75)
+xcal.calculate_x_value(0.90)
+xcal.calculate_x_value(0.95)
+
 plt.show()
