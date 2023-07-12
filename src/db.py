ff0d4ef515ec6a1c7d4c2d7357bf578bc73f6ee6
jericjan <cantilfrederick@gmail.com>
2023-07-13 01:03:17 +0800

    added sqlite database added saving and loaded simulations

---
+++ src/db.py
@@ -0,0 +1,75 @@
+import sqlite3
+import json
+
+class Database:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = sqlite3.connect(self.db_name)
+        self.create_table()
+
+    def create_table(self):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute('''CREATE TABLE IF NOT EXISTS "pulls" (
+                                "id" INTEGER,
+                                "name" TEXT,
+                                "rounds" INTEGER,
+                                "valk_pity" INTEGER,
+                                "gear_pity" INTEGER,
+                                "data" TEXT,
+                                PRIMARY KEY("id")
+                            )''')
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
+            self.conn.commit()
+
+    def count_items(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT COUNT(*) FROM pulls")
+        count = cursor.fetchone()[0]
+        cursor.close()
+        self.conn.commit()        
+        return count
+    
+    def add(self, name, rounds, valk_pity, gear_pity, data):
+        cursor = self.conn.cursor()
+        data_json = json.dumps(data)
+
+        # Prepare the query with the parameters
+        query = "INSERT INTO pulls (name, rounds, valk_pity, gear_pity, data) VALUES (?, ?, ?, ?, ?)"
+        params = (name, rounds, valk_pity, gear_pity, data_json)
+
+        # Execute the query with the parameters
+        cursor.execute(query, params)
+        cursor.close()
+        self.conn.commit()     
+
+    def show_all(self):        
+        cursor = self.conn.cursor()        
+        cursor.execute('SELECT id, name, rounds, valk_pity, gear_pity FROM pulls')    
+        rows = cursor.fetchall()
+        print(f"{'ID'.center(5)} | {'Name'.center(30)} | {'Rounds'.center(7)} | {'Valk Pity'.center(9)} | {'Gear Pity'.center(9)}")
+        for row in rows:
+            print(f"{str(row[0]).center(5)} | {str(row[1]).center(30)} | {str(row[2]).center(7)} | {str(row[3]).center(9)} | {str(row[4]).center(9)}")
+        cursor.close()
+        self.conn.commit()     
+        
+    def get_row(self, id):
+        cursor = self.conn.cursor()    
+
+        cursor.execute('SELECT data FROM pulls WHERE id = ?', (id,))
+        row = cursor.fetchone()        
+        cursor.close()
+        self.conn.commit()     
+
+        return row
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+
+
+            
--- src/main.py
+++ src/main.py
@@ -10,10 +10,16 @@ from threading import Thread
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import choice
+from db import Database
 
-ROUNDS = 10000
+DEFAULT_ROUNDS = 10000
+DEFAULT_VALK_PITY = 100
+DEFAULT_GEAR_PITY = 50
 logging.basicConfig(level=logging.INFO)
 SHOW_EFFICIENCY = False
+
+db = Database("saved_pulls.db")
+saved_pulls_count = db.count_items()
 
 class CustomThread(Thread):
     def __init__(
@@ -96,7 +102,7 @@ def make_bar(bins, values, title, labels=[]):
         ax = plt.gcf().axes[0]
         legend = ax.legend(prop={"size": 10})
 
-def pull_valk(amount_wanted=1, pity=100):
+def pull_valk(rounds, amount_wanted=1, pity=100):
     logging.info("Pulling valks")
     item_wanted = "valk"
     items = [item_wanted, "not"]
@@ -104,7 +110,7 @@ def pull_valk(amount_wanted=1, pity=100):
 
     pull_success_results = []
 
-    for x in range(ROUNDS):
+    for x in range(rounds):
         pull_count = 0
         item_got = ""
         amount_got = 0
@@ -128,7 +134,7 @@ def pull_valk(amount_wanted=1, pity=100):
     return pull_success_results
 
 
-def pull_gears(wishing_well=True, gear_pity=50):
+def pull_gears(rounds, wishing_well=True, gear_pity=50):
     if wishing_well:
         logging.info("Pulling gears w/ wishing well")
     else:
@@ -157,7 +163,7 @@ def pull_gears(wishing_well=True, gear_pity=50):
 
     pull_success_results = []
 
-    for idx, x in enumerate(range(ROUNDS)):
+    for idx, x in enumerate(range(rounds)):
         item_counts = dict.fromkeys(items, 0)
         pull_count = 0
         pity_count = 0
@@ -167,7 +173,7 @@ def pull_gears(wishing_well=True, gear_pity=50):
         last_round_logs = []
         while len(items_got) < len(items_wanted):
             last_round_logs.append(
-                f"=====ROUND {str(idx+1).ljust(len(str(ROUNDS)))}; PULL {str(pull_count).ljust(3)}; PITY {str(pity_count).ljust(3)}; START!!!======= {item_counts}"
+                f"=====ROUND {str(idx+1).ljust(len(str(rounds)))}; PULL {str(pull_count).ljust(3)}; PITY {str(pity_count).ljust(3)}; START!!!======= {item_counts}"
             )
             if pull_count > 200:
                 with open("log.txt", "w") as f:
@@ -202,7 +208,7 @@ def pull_gears(wishing_well=True, gear_pity=50):
                     wishing_well_msg = "(Wishing well-able)"
                     break
             last_round_logs.append(
-                f"=====ROUND {str(idx+1).ljust(len(str(ROUNDS)))}; PULL {str(pull_count).ljust(3)}; PITY {str(pity_count).ljust(3)}; END!!!======= {item_counts}"
+                f"=====ROUND {str(idx+1).ljust(len(str(rounds)))}; PULL {str(pull_count).ljust(3)}; PITY {str(pity_count).ljust(3)}; END!!!======= {item_counts}"
             )
 
         item_counts = dict(
@@ -220,182 +226,225 @@ def pull_gears(wishing_well=True, gear_pity=50):
         logging.info("Pulling gears w/o wishing well finished.")
     return pull_success_results
 
+def get_integer_input(prompt, default):
+    while True:
+        try:
+            user_input = input(f"{prompt} (default: {default})? ")
+            return int(user_input) if user_input else default
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
 
-save_file = Path("pull_data.json")
+def setup_info():
+    print("Leave blank to use the default value.")
+    name = input("Give this simulation a name: ") or "Unnamed"
+    
+    rounds = get_integer_input("How many rounds", DEFAULT_ROUNDS)
+    valk_pity = get_integer_input("What's the valkyrie pity", DEFAULT_VALK_PITY)
+    gear_pity = get_integer_input("What's the gear pity", DEFAULT_GEAR_PITY)
 
-if save_file.exists():
-    with Path("pull_data.json").open() as f:
-        dic = json.load(f)
-        valk_res = dic["valks"]
-        gear_res = dic["gears"]
-        no_well_gear_res = dic["no_well_gears"]
-else:
+    return name, rounds, valk_pity, gear_pity
+
+def new_sim():
+    name, rounds, valk_pity, gear_pity = setup_info()
+        
     t_starter = ThreadStarter([
-        CustomThread(target=pull_valk),
-        CustomThread(target=pull_gears, kwargs={"wishing_well": True}),
-        CustomThread(target=pull_gears, kwargs={"wishing_well": False}),
+        CustomThread(target=pull_valk, kwargs={"rounds": rounds, "pity": valk_pity}),
+        CustomThread(target=pull_gears, kwargs={"rounds": rounds, "wishing_well": True, "gear_pity": gear_pity}),
+        CustomThread(target=pull_gears, kwargs={"rounds": rounds, "wishing_well": False, "gear_pity": gear_pity}),
     ])
 
     t_starter.start()
 
     valk_res, gear_res, no_well_gear_res = t_starter.results
+    dic = {"valks": valk_res, "gears": gear_res, "no_well_gears": no_well_gear_res}
+    db.add(name, rounds, valk_pity, gear_pity, dic)
+    return valk_res, gear_res, no_well_gear_res
 
-    with Path("pull_data.json").open("w") as f:
-        dic = {"valks": valk_res, "gears": gear_res, "no_well_gears": no_well_gear_res}
-        json.dump(dic, f)
+def main():
+    if saved_pulls_count != 0:
+        db.show_all()
+        
+        while True:
+            start_choice = input("Type the ID to view a simulation or 'n' to run a new one: ")
+            if start_choice == "n":
+                valk_res, gear_res, no_well_gear_res = new_sim()
+                break
+            else:
+                row = db.get_row(start_choice)
+                if row:              
+                    row = json.loads(row[0])  
+                    valk_res = row["valks"]
+                    gear_res = row["gears"]
+                    no_well_gear_res = row["no_well_gears"]                
+                    break
+                else:
+                    print("ID doesn't exist.")
+    else:
+        print("No saved pulls yet.")
+        valk_res, gear_res, no_well_gear_res = new_sim()
 
-valk_res = np.array(valk_res)
-gear_res = np.array(gear_res)
-no_well_gear_res = np.array(no_well_gear_res)
 
-combined_res = valk_res + gear_res
-no_well_combined_res = valk_res + no_well_gear_res
+    valk_res = np.array(valk_res)
+    gear_res = np.array(gear_res)
+    no_well_gear_res = np.array(no_well_gear_res)
 
-valk_xtals = statistics.mean(valk_res) * 280
-gear_xtals = statistics.mean(gear_res) * 280
+    combined_res = valk_res + gear_res
+    no_well_combined_res = valk_res + no_well_gear_res
+
+    valk_xtals = statistics.mean(valk_res) * 280
+    gear_xtals = statistics.mean(gear_res) * 280
 
 
-def calc_mean_median_mode(data):
-    mean = statistics.mean(data)
-    print(f"- Mean: {mean:.2f} ({(mean*280):.2f} xtals)")
+    def calc_mean_median_mode(data):
+        mean = statistics.mean(data)
+        print(f"- Mean: {mean:.2f} ({(mean*280):.2f} xtals)")
 
-    median = statistics.median(data)
-    print(f"- Median: {median:.2f} ({(median*280):.2f} xtals)")
+        median = statistics.median(data)
+        print(f"- Median: {median:.2f} ({(median*280):.2f} xtals)")
 
-    mode = statistics.multimode(data)
+        mode = statistics.multimode(data)
+        print(
+            f"- Mode: {', '.join(str(x) for x in mode)} ({', '.join([str(x*280) for x in mode])} xtals)"
+        )
+
+        print(f"- Min: {min(data):.2f} Max: {max(data):.2f}")
+
+
+    print(f"Average pulls to get valk: {statistics.mean(valk_res)} ({valk_xtals} crystals)")
     print(
-        f"- Mode: {', '.join(str(x) for x in mode)} ({', '.join([str(x*280) for x in mode])} xtals)"
+        f"Average pulls to get all gear: {statistics.mean(gear_res)} ({gear_xtals} crystals)"
     )
+    print(f"Total crystals to 4/4 on average: {valk_xtals + gear_xtals}")
+    print("\n")
+    print("Valk statistics:")
+    calc_mean_median_mode(valk_res)
 
-    print(f"- Min: {min(data):.2f} Max: {max(data):.2f}")
+    print("\nGear statistics:")
+    print("With wishing well:")
+    calc_mean_median_mode(gear_res)
+    print("Without wishing well:")
+    calc_mean_median_mode(no_well_gear_res)
 
+    print("\nCombined statistics")
+    print("With wishing well:")
+    calc_mean_median_mode(combined_res)
+    print("Without wishing well:")
+    calc_mean_median_mode(no_well_combined_res)
 
-print(f"Average pulls to get valk: {statistics.mean(valk_res)} ({valk_xtals} crystals)")
-print(
-    f"Average pulls to get all gear: {statistics.mean(gear_res)} ({gear_xtals} crystals)"
-)
-print(f"Total crystals to 4/4 on average: {valk_xtals + gear_xtals}")
-print("\n")
-print("Valk statistics:")
-calc_mean_median_mode(valk_res)
-
-print("\nGear statistics:")
-print("With wishing well:")
-calc_mean_median_mode(gear_res)
-print("Without wishing well:")
-calc_mean_median_mode(no_well_gear_res)
-
-print("\nCombined statistics")
-print("With wishing well:")
-calc_mean_median_mode(combined_res)
-print("Without wishing well:")
-calc_mean_median_mode(no_well_combined_res)
-
-make_histogram(valk_res, "Valk pull successes", 10)
-make_histogram(gear_res, "Gear pull successes w/ wishing well")
+    make_histogram(valk_res, "Valk pull successes", 10)
+    make_histogram(gear_res, "Gear pull successes w/ wishing well")
 
 
-class Xcalculator:
-    def __init__(self, values, bins, labels=None):
-        self.values = values #contains y values
-        self.bins = bins #contains x values
-        self.labels = labels
-        self.dic = {x: [] for x in self.labels}
+    class Xcalculator:
+        def __init__(self, values, bins, labels=None):
+            self.values = values #contains y values
+            self.bins = bins #contains x values
+            self.labels = labels
+            self.dic = {x: [] for x in self.labels}
 
-    def calculate_x_value(self, y_value):
-        if isinstance(self.values, np.ndarray) and all(
-            isinstance(x, np.ndarray) for x in self.values
-        ):
-            for idx, x in enumerate(self.values):
-                index = np.where(x >= y_value)[0][0]
+        def calculate_x_value(self, y_value):
+            if isinstance(self.values, np.ndarray) and all(
+                isinstance(x, np.ndarray) for x in self.values
+            ):
+                for idx, x in enumerate(self.values):
+                    index = np.where(x >= y_value)[0][0]
+                    x_value = self.bins[index]
+                    label = self.labels[idx]
+                    self.dic[label].append(
+                        f"{x_value:.2f} pulls has a {y_value*100}% chance"
+                    )
+            else:
+                index = np.where(self.values >= y_value)[0][0]
                 x_value = self.bins[index]
-                label = self.labels[idx]
-                self.dic[label].append(
+                self.dic[self.labels[0]].append(
                     f"{x_value:.2f} pulls has a {y_value*100}% chance"
                 )
-        else:
-            index = np.where(self.values >= y_value)[0][0]
-            x_value = self.bins[index]
-            self.dic[self.labels[0]].append(
-                f"{x_value:.2f} pulls has a {y_value*100}% chance"
-            )
 
-    def calculate_y_value(self, x_value):
-        if isinstance(self.values, np.ndarray) and all(
-            isinstance(x, np.ndarray) for x in self.values
-        ):
-            index = np.where(self.bins >= x_value)[0][0]
-            for idx, x in enumerate(self.values):
-                y_value = x[index]
-                label = self.labels[idx]
-                self.dic[label].append(
+        def calculate_y_value(self, x_value):
+            if isinstance(self.values, np.ndarray) and all(
+                isinstance(x, np.ndarray) for x in self.values
+            ):
+                index = np.where(self.bins >= x_value)[0][0]
+                for idx, x in enumerate(self.values):
+                    y_value = x[index]
+                    label = self.labels[idx]
+                    self.dic[label].append(
+                        f"{x_value:.2f} pulls has a {(y_value*100):.2f}% chance"
+                    )
+            else:
+                index = np.where(self.bins >= x_value)[0][0]
+                y_value = self.values[index]            
+                self.dic[self.labels[0]].append(
                     f"{x_value:.2f} pulls has a {(y_value*100):.2f}% chance"
                 )
-        else:
-            index = np.where(self.bins >= x_value)[0][0]
-            y_value = self.values[index]            
-            self.dic[self.labels[0]].append(
-                f"{x_value:.2f} pulls has a {(y_value*100):.2f}% chance"
-            )
 
-    def show_all(self):
-        for key, values in self.dic.items():
-            print(key)
-            for value in values:
-                print(f"- {value}")
+        def show_all(self):
+            for key, values in self.dic.items():
+                print(key)
+                for value in values:
+                    print(f"- {value}")
 
 
-values, bins, labels = make_histogram(
-    valk_res, "Valk pull successes (cumulative)", cumulative=True
-)
-print("VALK ONLY:")
-xcal = Xcalculator(values, bins, ["Valk pulls"])
-xcal.calculate_x_value(0.25)
-xcal.calculate_x_value(0.5)
-xcal.calculate_x_value(0.75)
-xcal.calculate_x_value(0.90)
-xcal.calculate_x_value(0.95)
-xcal.show_all()
+    values, bins, labels = make_histogram(
+        valk_res, "Valk pull successes (cumulative)", cumulative=True
+    )
+    print("VALK ONLY:")
+    xcal = Xcalculator(values, bins, ["Valk pulls"])
+    xcal.calculate_x_value(0.25)
+    xcal.calculate_x_value(0.5)
+    xcal.calculate_x_value(0.75)
+    xcal.calculate_x_value(0.90)
+    xcal.calculate_x_value(0.95)
+    xcal.show_all()
 
-if SHOW_EFFICIENCY:
-    make_bar(bins, values, "Valk efficiency")
+    if SHOW_EFFICIENCY:
+        make_bar(bins, values, "Valk efficiency")
 
-values, bins, labels = make_histogram(
-    [gear_res, no_well_gear_res],
-    "Gear pull successes (cumulative)",
-    cumulative=True,
-    alpha=0.5,
-    label=["With wishing well:", "Without wishing well:"],
-)
-print("GEARS ONLY:")
-xcal = Xcalculator(values, bins, labels)
-xcal.calculate_x_value(0.25)
-xcal.calculate_x_value(0.5)
-xcal.calculate_x_value(0.75)
-xcal.calculate_x_value(0.90)
-xcal.calculate_x_value(0.95)
-xcal.show_all()
+    values, bins, labels = make_histogram(
+        [gear_res, no_well_gear_res],
+        "Gear pull successes (cumulative)",
+        cumulative=True,
+        alpha=0.5,
+        label=["With wishing well:", "Without wishing well:"],
+    )
+    print("GEARS ONLY:")
+    xcal = Xcalculator(values, bins, labels)
+    xcal.calculate_x_value(0.25)
+    xcal.calculate_x_value(0.5)
+    xcal.calculate_x_value(0.75)
+    xcal.calculate_x_value(0.90)
+    xcal.calculate_x_value(0.95)
+    xcal.show_all()
 
-if SHOW_EFFICIENCY:
-    make_bar(bins, values, "Gear efficiency", ["With wishing well:", "Without wishing well:"])
+    if SHOW_EFFICIENCY:
+        make_bar(bins, values, "Gear efficiency", ["With wishing well:", "Without wishing well:"])
 
-values, bins, labels = make_histogram(
-    [combined_res, no_well_combined_res],
-    "Combined pull successes (cumulative)",
-    cumulative=True,
-    alpha=0.5,
-    label=["With wishing well:", "Without wishing well:"],
-)
-print("COMBINED:")
-xcal = Xcalculator(values, bins, labels)
-xcal.calculate_x_value(0.25)
-xcal.calculate_x_value(0.5)
-xcal.calculate_x_value(0.75)
-xcal.calculate_x_value(0.90)
-xcal.calculate_x_value(0.95)
-xcal.show_all()
+    values, bins, labels = make_histogram(
+        [combined_res, no_well_combined_res],
+        "Combined pull successes (cumulative)",
+        cumulative=True,
+        alpha=0.5,
+        label=["With wishing well:", "Without wishing well:"],
+    )
+    print("COMBINED:")
+    xcal = Xcalculator(values, bins, labels)
+    xcal.calculate_x_value(0.25)
+    xcal.calculate_x_value(0.5)
+    xcal.calculate_x_value(0.75)
+    xcal.calculate_x_value(0.90)
+    xcal.calculate_x_value(0.95)
+    xcal.show_all()
 
-if SHOW_EFFICIENCY:
-    make_bar(bins, values, "Combined efficiency", ["With wishing well:", "Without wishing well:"])
+    if SHOW_EFFICIENCY:
+        make_bar(bins, values, "Combined efficiency", ["With wishing well:", "Without wishing well:"])
 
-plt.show()
+    print("Close all graph windows to continue...")
+
+    plt.show()
+
+
+try:
+    main()
+finally:
+    print("Closing database...")
+    db.close()
